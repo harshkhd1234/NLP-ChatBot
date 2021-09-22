2e1860dda8f38e80d55f2b66a2da28ba72b072cc
Harsh Khandelwal <43463476+khandelwalharsh@users.noreply.github.com>
2021-09-22 09:39:51 +0530

    Add files via upload

---
+++ run.py
@@ -0,0 +1,7 @@
+a=[]
+b=[]
+for i in range(0,10):
+    a.clear()
+    a.append(i)
+    b.append(a)
+    print(b[i])
---
+++ sample.py
@@ -0,0 +1,6 @@
+L=input()
+a=L.split()
+l=a[0] + " " + a[1]
+r=a[2] + " " + a[3]
+print(l)
+print(r)
